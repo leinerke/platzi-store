fb0ea4d072682c93c40418aa57293e6ea8b3c805
Leinerke Suarez <leinerke2000@gmail.com>
2022-04-14 21:15:44 -0500

    GET: recibir parámetros

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -18,5 +18,18 @@ export class AppController {
   @Get('/hello/')
   hello(): string {
     return 'Hello World!';
+  }
+
+  @Get('products/:id')
+  getProduct(@Param('id') id: string): string {
+    return `product ${id}`;
+  }
+
+  @Get('categories/:id/products/:productId')
+  getCategory(
+    @Param('id') id: string,
+    @Param('productId') productId: string,
+  ): string {
+    return `product ${id} and ${productId}`;
   }
 }
