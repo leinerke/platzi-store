b786c2bd74076c2d71df7ecc1925f6b2f992e735
Leinerke Suarez <leinerke2000@gmail.com>
2022-04-14 21:05:00 -0500

    Introduccion a controladores.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,14 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('nuevo')
+  getNuevo(): string {
+    return 'Hola mundo';
+  }
+
+  @Get('/hello/')
+  hello(): string {
+    return 'Hello World!';
+  }
 }
